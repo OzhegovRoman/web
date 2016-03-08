1253047e8386493ba86b71b47831ccaebdce1fd7
Roman <Ozhegov_Roman@mail.ru>
2016-03-08 16:41:32 +0300

    1.9

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -s /home/box/web/etc/nginx_lubuntu.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx_box.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
